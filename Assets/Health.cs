6d5d8407c5a2517652d9d96507379e28d665521e
Nicolas Martin <nicolas.martin.0986@gmail.com>
2017-03-29 21:56:10 -0400

    init up to single player health bars

---
+++ Assets/Health.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Health : MonoBehaviour
+{
+
+    public const int MaxHealth = 100;
+    public int CurrentHealth = MaxHealth;
+    public RectTransform HealthBar;
+
+    public void TakeDamage(int amount)
+    {
+        CurrentHealth -= amount;
+        if (CurrentHealth <= 0)
+        {
+            CurrentHealth = 0;
+            Debug.Log("Dead!");
+        }
+
+        HealthBar.sizeDelta = new Vector2(CurrentHealth, HealthBar.sizeDelta.y);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+    public GameObject BulletPrefab;
+    public Transform BulletSpawn;
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+
+
+        var axis = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        var x = axis * Time.deltaTime * 150.0f;
+        var z = vertical * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdFire();
+        }
+    }
+
+    [Command]
+    private void CmdFire()
+    {
+        var bullet = (GameObject)Instantiate(BulletPrefab, BulletSpawn.position, BulletSpawn.rotation);
+
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+        NetworkServer.Spawn(bullet);
+
+        Destroy(bullet, 2.0f);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+}
